bdd14b1d35cb29af6075cef8535264a2f643d990
Muhammad Fattah Ziidan <118587788+dann0204@users.noreply.github.com>
2024-05-23 22:50:11 +0700

    Update deleteMember.php

--- uasweb/DashboardAdmin/member/deleteMember.php
+++ uasweb/DashboardAdmin/member/deleteMember.php
@@ -1,8 +1,8 @@
 <?php 
 require "../../config/config.php"; 
 
-$nisnMember = $_GET["id"];
-if(deleteMember($nisnMember) > 0) {
+$npmMember = $_GET["id"];
+if(deleteMember($npmMember) > 0) {
     echo "<script>
     alert('Member berhasil dihapus!');
     document.location.href = 'member.php';
